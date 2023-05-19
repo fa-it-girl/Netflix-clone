4efb84e4028154917df0b5431a31cfb2d962cc5a
fakhriyya rahimova <rahimovafakhriyya@gmail.com>
2023-05-19 11:22:43 +0200

    last version of netflix clone

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,7 +12,7 @@ const Navbar = () => {
   }
    return(
     <div className="navbar">
-     <Link to='/'><img src='https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png' alt='logo' className='img-fluid'></img></Link>
+     <Link to='/'><img src='https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897231c7763c/4345ea78-053c-46d2-b11e-09adaef973dc/Netflix_Logo_PMS.png' alt='logo' className='img-fluid'></img></Link>
      <Link to='/signin'><button onClick= {toggleButtonHandler} className='btn'>Sing in</button></Link>
     </div>
    )
